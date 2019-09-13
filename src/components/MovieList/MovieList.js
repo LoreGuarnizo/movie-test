68ea52f5440603387a09ef4282f21d3eedc393fa
German Hernandez <muygrafico@gmail.com>
2019-09-13 15:13:09 -0500

    use Link element instead of a redirect | small re-factor

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,34 +1,17 @@
-import React, {useState, Fragment} from 'react';
-import { Redirect } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './MovieList.scss';
-import {IMAGE_BASE_URL} from '../../helpers/constants';
+import { IMAGE_BASE_URL } from '../../helpers/constants';
 
-const MovieList = props => {
-  const [id, setId] = useState(0);
-
-  const goToDetail = id => {
-    setId(id);
-  }
-
-  const renderRedirect = () => {
-    if (id) {
-      return <Redirect to={`/detail/${id}`} />
-    }
-  }
-  
-  return (
-    <Fragment>
-      {renderRedirect()}
-      <div className="movie-container">
-        {props.movies.map((movie) =>
-          (<div key={movie.id} className="movie-card" onClick={() => goToDetail(movie.id)}>
-            <img alt='img' key={movie.id} src={`${IMAGE_BASE_URL}${movie.poster_path}`} />
-          </div>))
-        } 
-      </div>
-    </Fragment>
-  )
-};
+const MovieList = (props) => (
+	<div className="movie-container">
+    {props.movies.map((movie) => (
+      <Link to={`/detail/${movie.id}`} key={movie.id} className="movie-card">
+        <img alt="img" src={`${IMAGE_BASE_URL}${movie.poster_path}`} />
+      </Link>
+    ))}
+	</div>
+);
 
 export default MovieList;
